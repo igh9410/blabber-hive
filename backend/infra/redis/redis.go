f5a7d392efbe5e1ef2872caf0010687dbdf24c8b
Geonhyuk Im <athanasia9410@gmail.com>
2023-12-01 15:23:33 +0900

    Fixed Redis connection issue in Production

--- backend/infra/redis/redis.go
+++ backend/infra/redis/redis.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"log"
+	"log/slog"
 	"os"
 
 	"github.com/redis/go-redis/v9"
@@ -13,25 +14,29 @@ type RedisClient struct {
 }
 
 func NewRedisClient() (*RedisClient, error) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv(("REDIS_URL")),
-		Password: os.Getenv("REDIS_PASSWORD"), // no password set for local
-		DB:       0,                           // use default DB
-	})
+	// Construct the Redis URL from environment variables
+	redisURL := os.Getenv("REDIS_URL") //
+
+	// Parse the Redis URL
+	opt, err := redis.ParseURL(redisURL)
+	if err != nil {
+		slog.Error("Failed to parse Redis URL: %s\n", err)
+	}
+
+	client := redis.NewClient(opt)
 
 	// Create a background context for the Ping operation
 	ctx := context.Background()
 
 	// Ping the Redis server to check for a successful connection
-	_, err := rdb.Ping(ctx).Result()
+	_, err = client.Ping(ctx).Result()
 	if err != nil {
 		log.Printf("Failed to ping Redis server: %s\n", err)
 		return nil, err
 	}
 	log.Println("Redis initialized")
-	return &RedisClient{Client: rdb}, nil
+	return &RedisClient{Client: client}, nil
 }
-
 func (r *RedisClient) Close() error {
 	return r.Client.Close()
 }
