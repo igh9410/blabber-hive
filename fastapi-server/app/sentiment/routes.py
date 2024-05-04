3468bc87d1b377c9fb8498f5251e498bea5cdb78
igh9410 <athanasia9410@gmail.com>
2024-05-04 23:21:13 +0900

    Configured Model Server, Nginx and restored Postgres data

---
+++ fastapi-server/app/sentiment/routes.py
@@ -0,0 +1,18 @@
+# app/sentiment/routes.py
+from fastapi import APIRouter, HTTPException
+from app.sentiment.models import SentimentRequest, SentimentResponse
+from app.sentiment.crud import analyze_sentiment
+
+sentiment_router = APIRouter()
+
+
+@sentiment_router.post("/sentiments/", response_model=SentimentResponse)
+async def sentiment_analysis(request: SentimentRequest):
+    """
+    sentiment_analysis endpoint takes a SentimentRequest as input
+    """
+    try:
+        sentiment = analyze_sentiment(request.text)
+        return SentimentResponse(sentiment=sentiment)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
